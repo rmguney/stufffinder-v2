dd82da9b3cbe5263264c8f0921a58321d61d131c
mustafa-bektas <151761340+mustafa-bektas@users.noreply.github.com>
2025-02-22 19:27:29 +0300

    test fixes

--- backend/src/main/java/com/swe574/group2/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/swe574/group2/backend/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.swe574.group2.backend.config;
 
 import com.swe574.group2.backend.security.JwtRequestFilter;
-import com.swe574.group2.backend.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
--- backend/src/main/java/com/swe574/group2/backend/config/WebConfig.java
+++ backend/src/main/java/com/swe574/group2/backend/config/WebConfig.java
@@ -1,8 +1,7 @@
 package com.swe574.group2.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration

0b12751ad8bbf67bb15f7d5f5b11c034842d393c
MertUnlu-SWE <mert.unlu@std.bogazici.edu.tr>
2025-04-05 14:20:24 +0300

    better post display for the main page #138
    * Resolved merge conflicts at #166
    
    * Added display items, vote count and comment count needs information from backend
    
    * added comment count and vote counts for displaying them in main page posts
    
    * post size fix in main page

--- backend/src/main/java/com/swe574/group2/backend/dto/PostListDto.java
+++ backend/src/main/java/com/swe574/group2/backend/dto/PostListDto.java
@@ -15,15 +15,22 @@ public class PostListDto {
     private String mysteryObjectImageUrl;
     private boolean isSolved;
     private LocalDateTime createdAt;
+    private int commentCount;
+    private int upvotesCount;
+    private int downvotesCount;
 
-    public PostListDto(Long id, String author, String title, String description, String mysteryObjectImageUrl, boolean isSolved) {
+    public PostListDto(Long id, String author, String title, String description, String mysteryObjectImageUrl, boolean isSolved, int upvotesCount, int downvotesCount, Long commentCount) {
         this.id = id;
         this.author = author;
         this.title = title;
         this.description = description;
         this.mysteryObjectImageUrl = mysteryObjectImageUrl;
         this.isSolved = isSolved;
+        this.upvotesCount = upvotesCount;
+        this.downvotesCount = downvotesCount;
+        this.commentCount = commentCount != null ? commentCount.intValue() : 0;
     }
+    
 
     public PostListDto() {
     }
